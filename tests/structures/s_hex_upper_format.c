121c40b36b8b610e864b5880f2838a0185a4b186
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-10-04 13:55:57 -0300

    remove leak de memoria

--- tests/structures/s_hex_upper_format.c
+++ tests/structures/s_hex_upper_format.c
@@ -8,6 +8,7 @@ static void	func_testing_get_arg_hex_upper(t_data_format *data_format, ...)
 	va_start(ap, data_format);
 	int	*nb = data_format->get_arg(ap);
 	mu_check(*nb == 123);
+	free(nb);
 	va_end(ap);
 }
 
@@ -20,17 +21,24 @@ MU_TEST(testing_get_arg_hex_upper) {
 MU_TEST(test_hex_upper_formatter) {
 	t_data_format	*data_format = get_hex_upper_format();
 	unsigned long	n;
+	char			*result;
 
 	mu_check(data_format != NULL);
 
 	n = -1;
-	mu_check(!ft_strncmp(data_format->formatter(&n), "FFFFFFFFFFFFFFFF", 16));
+	result = data_format->formatter(&n);
+	mu_check(!ft_strncmp(result, "FFFFFFFFFFFFFFFF", 16));
+	free(result);
 
 	n = 10;
-	mu_check(!ft_strncmp(data_format->formatter(&n), "A", 1));
+	result = data_format->formatter(&n);
+	mu_check(!ft_strncmp(result, "A", 1));
+	free(result);
 
 	n = 16;
-	mu_check(!ft_strncmp(data_format->formatter(&n), "10", 1));
+	result = data_format->formatter(&n);
+	mu_check(!ft_strncmp(result, "10", 1));
+	free(result);
 
 	free(data_format);
 }
