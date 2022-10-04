198017924e3d9af61f3c9f753921bb8ba45eaf7f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-10-04 14:38:39 -0300

    implementa get_pointer_format

---
+++ sources/formats/get_pointer_format.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_pointer_format.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfanucch <dfanucch@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 13:15:23 by dfanucch          #+#    #+#             */
+/*   Updated: 2022/10/04 13:15:23 by dfanucch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_printf.h>
+
+static char	*formatter(void *data)
+{
+	t_hex_base_converter	*converter;
+	char					*prefix;
+	char					*hex;
+	char					*result;
+
+	result = NULL;
+	converter = get_hex_base_converter();
+	if (converter)
+	{
+		prefix = ft_strdup("0x");
+		hex = converter->convert(
+				(unsigned long) data,
+				converter->hex_lower_digits);
+		result = ft_strjoin(prefix, hex);
+		free(prefix);
+		free(hex);
+		free(converter);
+	}
+	return (result);
+}
+
+static void	*get_arg(va_list ap)
+{
+	return (va_arg(ap, void *));
+}
+
+t_data_format	*get_pointer_format(void)
+{
+	t_data_format	*data_format;
+
+	data_format = malloc(sizeof(t_data_format));
+	if (data_format)
+	{
+		data_format->formatter = formatter;
+		data_format->get_arg = get_arg;
+	}
+	return (data_format);
+}
--- tests/main.c
+++ tests/main.c
@@ -3,6 +3,7 @@
 #include "./structures/s_str_format.c"
 #include "./structures/s_hex_lower_format.c"
 #include "./structures/s_hex_upper_format.c"
+#include "./structures/s_pointer_format.c"
 #include "./test_hex_base_converter.c"
 
 int	main()
@@ -12,6 +13,7 @@ int	main()
 	MU_RUN_SUITE(test_hex_base_converter);
 	MU_RUN_SUITE(test_hex_lower_format);
 	MU_RUN_SUITE(test_hex_upper_format);
+	MU_RUN_SUITE(test_pointer_format);
 	MU_REPORT();
 
 	return (0);
