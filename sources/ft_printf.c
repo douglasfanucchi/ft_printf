6456e9086b7f4fadb80aa1984fbb59338bc697d4
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-10-07 08:38:49 -0300

    print_f retorna quantidade de bytes printados

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -12,43 +12,47 @@
 
 #include <ft_printf.h>
 
-static void	ft_print_formatted_data(t_list **formats,
-	t_data_format *data_format, va_list ap)
+static size_t	format_and_print(t_data_format *data_format, va_list ap)
 {
-	char			*arg;
-	char			*formatted;
+	char	*arg;
+	char	*formatted;
+	size_t	len;
 
 	arg = NULL;
 	if (data_format->get_arg)
 		arg = data_format->get_arg(ap);
 	formatted = data_format->formatter(arg);
-	ft_pustr(formatted);
+	ft_putstr(formatted);
+	len = ft_strlen(formatted);
 	free(formatted);
 	free(arg);
+	return (len);
 }
 
-void	ft_printf(char *str, ...)
+size_t	ft_printf(char *str, ...)
 {
 	t_list			**formats;
 	t_data_format	*data_format;
 	va_list			ap;
+	size_t			len;
 
+	len = 0;
 	va_start(ap, str);
 	formats = get_formats_list();
-	if (!formats)
-		return ;
 	while (*str)
 	{
 		data_format = find_format_from_str_start(formats, str);
 		if (data_format)
 		{
-			ft_print_formatted_data(formats, data_format, ap);
+			len += format_and_print(data_format, ap);
 			str += ft_strlen(data_format->id);
 			continue ;
 		}
 		write(1, str++, 1);
+		len++;
 	}
 	ft_lstclear(formats, del_data_formats);
 	free(formats);
 	va_end(ap);
+	return (len);
 }
